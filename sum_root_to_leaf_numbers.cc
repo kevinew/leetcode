dfe71051bcaeaabc361f38f754fe08a6bbb2e1fe
Wenkai Liu <kevinew.1221@gmail.com>
2014-03-17 13:54:03 +0800

    Update sum_root_to_leaf_numbers.cc

--- sum_root_to_leaf_numbers.cc
+++ sum_root_to_leaf_numbers.cc
@@ -11,8 +11,8 @@ class Solution {
   public:
     int sumNumbers(TreeNode *root) {
       if (NULL == root) return 0;
-      int l_val = sumNumbers(root->left->val);
-      int r_val = sumNumbers(root->right->val);
+      int l_val = sumNumbers(root->left);
+      int r_val = sumNumbers(root->right);
       int val = l_val + r_val;
       if (val) return root->val * 10 + val;
       else return root->val;
