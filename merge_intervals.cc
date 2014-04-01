e6e9028762f34b4eeb832bd73b9dae52a1e83a87
Wenkai Liu <kevinew.1221@gmail.com>
2014-04-01 18:54:27 +0800

    Update merge_intervals.cc

--- merge_intervals.cc
+++ merge_intervals.cc
@@ -8,14 +8,27 @@
  * };
  */
 
-bool cmp(const Interval& a, const Interval& b) {
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+
+bool cmp(const Interval &a, const Interval &b) {
   return a.start < b.start;
 }
 
 class Solution {
   public:
     vector<Interval> merge(vector<Interval> &intervals) {
-      sort(intervals.begin(), intervals.end());
+      sort(intervals.begin(), intervals.end(), cmp);
 
       vector<Interval> res;
       if (intervals.size() == 0) return res;
